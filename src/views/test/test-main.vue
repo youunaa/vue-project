1782245a75576a1fc5707dddd021bb3d510fe7c0
youunaa <yn060312@gmail.com>
2022-08-11 14:21:41 +0900

    웹소켓 적용 및 테스트 페이지 작성

--- src/views/test/test-main.vue
+++ src/views/test/test-main.vue
@@ -1,22 +1,88 @@
 <template>
   <div>
     <h2>WebSocket Test</h2>
-    <div id="output"></div>
+    <div id="output">
+      유저이름:
+      <input type="text" v-model="userName">
+      내용:
+      <input type="text" v-model="message" @keyup="sendMessage">
+
+      <div v-for="(item, idx) in recvList" :key="idx">
+        <h3>유저이름: {{ item.userName }}</h3>
+        <h3>내용: {{ item.content }}</h3>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
+import Stomp from 'webstomp-client'
+import SockJS from 'sockjs-client'
+
 export default {
   name: 'test-main'
   , data() {
-    return {}
+    return {
+      userName: "",
+      message: "",
+      recvList: []
+    }
   },
   created() {
-    this.$socket.emit('connect', 'data')
+    // 소켓 연결
+    this.connect();
   },
   methods: {
+
+    connect() {
+      const serverURL = "http://localhost:8080";
+      let socket = new SockJS(serverURL);
+      this.stompClient = Stomp.over(socket);
+
+      console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverURL}`)
+
+      this.stompClient.connect({}, frame => {
+          // 소켓 연결 성공
+          this.connected = true;
+
+          console.log('소켓 연결 성공', frame);
+
+          // 서버의 메시지 전송 endpoint를 구독합니다.
+          // 이런형태를 pub sub 구조라고 합니다.
+          this.stompClient.subscribe("/send", res => {
+            console.log('구독으로 받은 메시지 입니다.', res.body);
+
+            // 받은 데이터를 json으로 파싱하고 리스트에 넣어줍니다.
+            this.recvList.push(JSON.parse(res.body))
+          });
+        },
+        error => {
+          // 소켓 연결 실패
+          console.log('소켓 연결 실패', error);
+          this.connected = false;
+        }
+      );
+    },
+
+    sendMessage(e) {
+      if (e.keyCode === 13 && this.userName !== '' && this.message !== '') {
+        this.send();
+        this.message = ''
+      }
+    },
+
+    send() {
+      console.log("Send message:" + this.message);
+      if (this.stompClient && this.stompClient.connected) {
+        const msg = {
+          userName: this.userName,
+          content: this.message
+        };
+        this.stompClient.send("/receive", JSON.stringify(msg), {});
+      }
+    },
 
   }
 };
 </script>
-
