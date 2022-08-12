c7134594e22e8ff2f8e4bf709f890e4319702710
youunaa <yn060312@gmail.com>
2022-08-12 17:14:50 +0900

    chart.js 적용 - Bar 차트 demo 적용

--- src/views/dashboard/components/dashboard-daily.vue
+++ src/views/dashboard/components/dashboard-daily.vue
@@ -9,18 +9,100 @@
     </div>
     <div class="card-body">
       <div class="chart-area">
-        <canvas id="CountryChart"></canvas>
+        <canvas id="myChart" ref="lineChart" width="400" height="400"></canvas>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Chart from "chart.js";
+
 export default {
   data() {
-    return {};
+    return {
+      Chart,
+      gradientBarChartConfiguration : {
+        maintainAspectRatio: false,
+        legend: {
+          display: false
+        },
+        tooltips: {
+          backgroundColor: '#f5f5f5',
+          titleFontColor: '#333',
+          bodyFontColor: '#666',
+          bodySpacing: 4,
+          xPadding: 12,
+          mode: "nearest",
+          intersect: 0,
+          position: "nearest"
+        },
+        responsive: true,
+        scales: {
+          yAxes: [{
+            gridLines: {
+              drawBorder: false,
+              color: 'rgba(29,140,248,0.1)',
+              zeroLineColor: "transparent",
+            },
+            ticks: {
+              suggestedMin: 60,
+              suggestedMax: 120,
+              padding: 20,
+              fontColor: "#9e9e9e"
+            }
+          }],
+          xAxes: [{
+            gridLines: {
+              drawBorder: false,
+              color: 'rgba(29,140,248,0.1)',
+              zeroLineColor: "transparent",
+            },
+            ticks: {
+              padding: 20,
+              fontColor: "#9e9e9e"
+            }
+          }]
+        }
+      }
+    };
   },
+  mounted() {
+    this.drawChart();
+  },
+  methods: {
+    drawChart() {
+      const ctx = this.$refs.lineChart.getContext("2d");
 
-  methods: {},
+      var gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+
+      gradientStroke.addColorStop(1, 'rgba(29,140,248,0.2)');
+      gradientStroke.addColorStop(0.4, 'rgba(29,140,248,0.0)');
+      gradientStroke.addColorStop(0, 'rgba(29,140,248,0)'); //blue colors
+
+      var myChart = new Chart(ctx, {
+        type: 'bar',
+        responsive: true,
+        legend: {
+          display: false
+        },
+        data: {
+          labels: ['USA', 'GER', 'AUS', 'UK', 'RO', 'BR'],
+          datasets: [{
+            label: "Countries",
+            fill: true,
+            backgroundColor: gradientStroke,
+            hoverBackgroundColor: gradientStroke,
+            borderColor: '#1f8ef1',
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            data: [53, 20, 10, 80, 100, 45],
+          }]
+        },
+        options: this.gradientBarChartConfiguration
+      });
+    },
+  },
 };
 </script>
--- src/views/test/test-main.vue
+++ src/views/test/test-main.vue
@@ -7,9 +7,8 @@
       내용:
       <input type="text" v-model="message" @keyup="sendMessage">
 
-      <div v-for="(item, idx) in recvList" :key="idx">
-        <h3>유저이름: {{ item.userName }}</h3>
-        <h3>내용: {{ item.content }}</h3>
+      <div v-for="(item, idx) in recvList" :key="idx" >
+        <strong style="font-size: 20px; color: white">{{ item.userName }}</strong> 님 : <strong>{{ item.content }}</strong>
       </div>
 
     </div>
