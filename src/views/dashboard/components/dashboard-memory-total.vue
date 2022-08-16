f5a6cc504f82cd81acfa6c899b9b33a7a6812585
youunaa <yn060312@gmail.com>
2022-08-16 16:33:46 +0900

    대시보드 메인컴포넌트 - metric 데이터 웹소켓 요청 후 자식 컴포넌트(cpu, memory 컴포넌트) 데이터 props 전달

--- src/views/dashboard/components/dashboard-memory-total.vue
+++ src/views/dashboard/components/dashboard-memory-total.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="card card-chart">
     <div class="card-header">
-      <h3 class="card-title">
-        <i class="tim-icons icon-send text-success"></i> CPU Usage
-      </h3>
+      <h4 class="card-title">
+        <i class="tim-icons icon-send text-success"></i> 물리적 메모리의 총량(byte)
+      </h4>
     </div>
     <div class="card-body">
-      <Progress :radius="50" :strokeWidth="10" value="86.12">
+      <Progress :radius="50" :strokeWidth="10" :value="value">
         <template v-slot:footer>
         </template>
       </Progress>
@@ -18,6 +18,9 @@
 import Progress from 'easy-circular-progress';
 
 export default {
+  props: {
+    value: [String, Number]
+  },
   data() {
     return {};
   },
--- src/views/dashboard/dashboard-main.vue
+++ src/views/dashboard/dashboard-main.vue
@@ -11,13 +11,19 @@
         </div>
         <div class="row">
           <div class="col-lg-4">
-            <dashboard-cpu />
+            <dashboard-cpu
+              :value="cpuVal"
+            />
           </div>
           <div class="col-lg-4">
-            <dashboard-memory-size />
+            <dashboard-memory-size
+              :value="memorySize"
+            />
           </div>
           <div class="col-lg-4">
-            <dashboard-memory-total />
+            <dashboard-memory-total
+              :value="memoryTotal"
+            />
           </div>
         </div>
         <div class="row">
@@ -54,6 +60,8 @@ import dashboardMemorySize from "@/views/dashboard/components/dashboard-memory-s
 import dashboardMemoryTotal from "@/views/dashboard/components/dashboard-memory-total";
 import dashboardExample2 from "@/views/dashboard/components/dashboard-example2";
 import dashboardExample3 from "@/views/dashboard/components/dashboard-example3";
+import Stomp from 'webstomp-client';
+import SockJS from 'sockjs-client';
 
 export default {
   components: {
@@ -67,5 +75,58 @@ export default {
     dashboardMemorySize,
     dashboardMemoryTotal
   },
+  data() {
+    return {
+      cpuVal: 0,
+      memorySize: 0,
+      memoryTotal: 0,
+    }
+  },
+  created() {
+    // 소켓 연결
+    this.connect();
+    setInterval(this.send, 5000);
+  },
+  methods: {
+    connect() {
+      const serverURL = "http://localhost:8080";
+      let socket = new SockJS(serverURL);
+      this.stompClient = Stomp.over(socket);
+
+      console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverURL}`)
+
+      this.stompClient.connect({}, frame => {
+          // 소켓 연결 성공
+          this.connected = true;
+
+          console.log('소켓 연결 성공', frame);
+
+          // 서버의 메시지 전송 endpoint를 구독합니다.(pub/sub 구조)
+          this.stompClient.subscribe("/send", res => {
+            const obj = JSON.parse(res.body);
+            this.cpuVal = obj.cpuVal;
+            this.memorySize = obj.memorySize;
+            this.memoryTotal = obj.memoryTotal;
+          });
+        },
+        error => {
+          // 소켓 연결 실패
+          console.log('소켓 연결 실패', error);
+          this.connected = false;
+        }
+      );
+    },
+
+    send() {
+      console.log("Send message:" + this.message);
+      if (this.stompClient && this.stompClient.connected) {
+        const msg = {
+          userName: this.userName,
+          content: this.message
+        };
+        this.stompClient.send("/receive", JSON.stringify(msg), {});
+      }
+    },
+  }
 };
 </script>
