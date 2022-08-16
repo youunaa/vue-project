cc9d43788369557f7bcb213356e32b0fc1ae69ac
youunaa <yn060312@gmail.com>
2022-08-16 21:39:34 +0900

    prometheus 메트릭 차트 추가 - process_open_fds 조회

--- src/views/dashboard/components/dashboard-noti.vue
+++ src/views/dashboard/components/dashboard-noti.vue
@@ -1,25 +1,133 @@
 <template>
   <div class="card card-chart">
     <div class="card-header">
-      <h5 class="card-category">Total Shipments</h5>
-      <h3 class="card-title">
-        <i class="tim-icons icon-bell-55 text-primary"></i> 763,215
-      </h3>
+      <h5 class="card-category">Completed Tasks</h5>
+      <h3 class="card-title"><i class="tim-icons icon-send text-success"></i> 12,100K</h3>
     </div>
     <div class="card-body">
       <div class="chart-area">
-        <canvas id="chartLinePurple"></canvas>
+        <canvas id="chartLineGreen"></canvas>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Chart from "chart.js";
+import Progress from 'easy-circular-progress';
+import axios from "axios";
+
 export default {
   data() {
-    return {};
+    return {
+      Chart,
+      chart_labels: [],
+      chart_data: [],
+      gradientChartOptionsConfigurationWithTooltipGreen: {
+        maintainAspectRatio: false,
+        legend: {
+          display: false
+        },
+        tooltips: {
+          backgroundColor: '#f5f5f5',
+          titleFontColor: '#333',
+          bodyFontColor: '#666',
+          bodySpacing: 4,
+          xPadding: 12,
+          mode: "nearest",
+          intersect: 0,
+          position: "nearest"
+        },
+        responsive: true,
+        scales: {
+          yAxes: [{
+            barPercentage: 1.6,
+            gridLines: {
+              drawBorder: false,
+              color: 'rgba(29,140,248,0.0)',
+              zeroLineColor: "transparent",
+            },
+            ticks: {
+              suggestedMin: 50,
+              // suggestedMax: 125,
+              padding: 20,
+              fontColor: "#9e9e9e"
+            }
+          }],
+          xAxes: [{
+            barPercentage: 10,
+            gridLines: {
+              drawBorder: false,
+              color: 'rgba(0,242,195,0.1)',
+              zeroLineColor: "transparent",
+            },
+            ticks: {
+              padding: 20,
+              fontColor: "#9e9e9e"
+            }
+          }]
+        }
+      }
+    };
   },
+  mounted() {
+    this.reqPrometheus();
+    this.drawChart();
+  },
+  components: {
+    Progress
+  },
+  methods: {
+    reqPrometheus() {
+      axios.get('http://34.125.109.178:9090/api/v1/query_range?query=process_open_fds&start=1660646731.242&end=1660650331.242&step=14&_=1660650330889')
+        .then(response => {
+          response.data.data.result[0].values.forEach((cell, index) => {
+            this.chart_labels.push(cell[1])
+            this.chart_data.push(cell[10])
+          });
+        })
+    },
+    drawChart() {
+      // x축
+      var chart_labels = this.chart_data;
+      // y축
+      var chart_data = this.chart_labels;
 
-  methods: {},
+      var ctx = document.getElementById("chartLineGreen").getContext("2d");
+
+      var gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+
+      gradientStroke.addColorStop(1, 'rgba(66,134,121,0.15)');
+      gradientStroke.addColorStop(0.4, 'rgba(66,134,121,0.0)'); //green colors
+      gradientStroke.addColorStop(0, 'rgba(66,134,121,0)'); //green colors
+
+      var data = {
+        labels: chart_labels,
+        datasets: [{
+          label: "My First dataset",
+          fill: true,
+          backgroundColor: gradientStroke,
+          borderColor: '#00d6b4',
+          borderWidth: 2,
+          borderDash: [],
+          borderDashOffset: 0.0,
+          pointBackgroundColor: '#00d6b4',
+          pointBorderColor: 'rgba(255,255,255,0)',
+          pointHoverBackgroundColor: '#00d6b4',
+          pointBorderWidth: 20,
+          pointHoverRadius: 4,
+          pointHoverBorderWidth: 15,
+          pointRadius: 4,
+          data: chart_data,
+        }]
+      };
+
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: this.gradientChartOptionsConfigurationWithTooltipGreen
+      });
+    }
+  },
 };
 </script>
--- src/views/dashboard/components/dashboard-total.vue
+++ src/views/dashboard/components/dashboard-total.vue
@@ -129,13 +129,8 @@ export default {
     drawChart() {
       // x축
       var chart_labels = this.chart_data;
-      // // y축
+      // y축
       var chart_data = this.chart_labels;
-
-      console.log(chart_labels)
-      console.log(chart_data)
-      // var chart_labels = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
-      // var chart_data = [100, 70, 90, 70, 85, 60, 75, 60, 90, 80, 110, 100];
 
       var ctx = document.getElementById("chartBig1").getContext('2d');
 
@@ -175,21 +170,21 @@ export default {
         data.labels = chart_labels;
         myChartData.update();
       });
-      // $("#1").click(function () {
-      //   var chart_data = [80, 120, 105, 110, 95, 105, 90, 100, 80, 95, 70, 120];
-      //   var data = myChartData.config.data;
-      //   data.datasets[0].data = chart_data;
-      //   data.labels = chart_labels;
-      //   myChartData.update();
-      // });
-      //
-      // $("#2").click(function () {
-      //   var chart_data = [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130];
-      //   var data = myChartData.config.data;
-      //   data.datasets[0].data = chart_data;
-      //   data.labels = chart_labels;
-      //   myChartData.update();
-      // });
+      $("#1").click(function () {
+        var chart_data = [80, 120, 105, 110, 95, 105, 90, 100, 80, 95, 70, 120];
+        var data = myChartData.config.data;
+        data.datasets[0].data = chart_data;
+        data.labels = chart_labels;
+        myChartData.update();
+      });
+
+      $("#2").click(function () {
+        var chart_data = [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130];
+        var data = myChartData.config.data;
+        data.datasets[0].data = chart_data;
+        data.labels = chart_labels;
+        myChartData.update();
+      });
     }
   },
 };
